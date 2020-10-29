4ac625d9badada29dc18786efc13567e31d877fd
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-30 00:51:30 +0530

    [Anuj] Master Program

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InvoiceGenerator invoicegenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoicegenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare :{ fare}");
+        }
+    }
+}
