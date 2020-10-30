e2247eb1af3d0cc85873247de70545b38c2fc070
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-30 08:20:13 +0530

    [Anuj] Added premium and normal

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -6,12 +6,18 @@ namespace CabInvoiceGeneratorTest
     public class Tests
     {
         CabInvoiceGenerator.InvoiceGenerator invoiceGenerator = null;
+        /// <summary>
+        /// Setups this instance.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
 
         }
 
+        /// <summary>
+        /// Givens the distance and time when passed shoul retrun fare.
+        /// </summary>
         [Test]
         public void GivenDistanceAndTime_WhenPassed_ShoulRetrunFare()
         {
@@ -24,6 +30,9 @@ namespace CabInvoiceGeneratorTest
             Assert.AreEqual(expected_Fare,fare);
         }
 
+        /// <summary>
+        /// Givens the multiple rides when passed should return aggregate total.
+        /// </summary>
         [Test]
         public void GivenMultipleRides_WhenPassed_ShouldReturnAggregateTotal()
         {
@@ -36,6 +45,9 @@ namespace CabInvoiceGeneratorTest
             Assert.AreEqual(expected_Total_Fare, total_Fare);
         }
 
+        /// <summary>
+        /// Givens the multiple rides when passed should return invoice summary.
+        /// </summary>
         [Test]
         public void GivenMultipleRides_WhenPassed_ShouldReturnInvoiceSummary()
         {
@@ -48,6 +60,9 @@ namespace CabInvoiceGeneratorTest
             Assert.That(expected_Summary, Is.EqualTo(summary));
         }
 
+        /// <summary>
+        /// Givens the user identifier when passed should return invoice.
+        /// </summary>
         [Test]
         public void GivenUserID_WhenPassed_ShouldReturnInvoice()
         {
@@ -57,14 +72,39 @@ namespace CabInvoiceGeneratorTest
             rideRepository.AddRide("102", new Ride[] { new Ride(2.0, 7), new Ride(0.1, 6) });
             rideRepository.AddRide("103", new Ride[] { new Ride(2.0, 2), new Ride(0.1, 4) });
 
-            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-
             string given_UserID = "102";
             
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rideRepository.GetRides(given_UserID));
 
             InvoiceSummary expected_Summary = new InvoiceSummary { total_Fare = 34, total_Rides = 2, avg_Fare = 17 };
             Assert.That(expected_Summary, Is.EqualTo(summary));
+        }
+
+
+        [Test]
+        public void GivenRideTypePREMIUMandNormal_WhenPassed_ShouldReturnInvoice()
+        {
+
+            CabInvoiceGenerator.InvoiceGenerator invoiceGenerator_Normal = new CabInvoiceGenerator.InvoiceGenerator(RideType.NORMAL);
+            CabInvoiceGenerator.InvoiceGenerator invoiceGenerator_Premium = new CabInvoiceGenerator.InvoiceGenerator(RideType.PREMIUM);
+
+            double distance_Normal = 2.0;
+            int time_Normal = 5;
+            double distance_Premium = 2.0;
+            int time_Premium = 5;
+
+            double fare_Normal = invoiceGenerator_Normal.CalculateFare(distance_Normal, time_Normal);
+            double fare_Premium = invoiceGenerator_Premium.CalculateFare(distance_Premium, time_Premium);
+
+            double expected_Fare_Normal = 25;
+            double expected_Fare_Premium = 40;
+
+            Assert.AreEqual(expected_Fare_Normal, fare_Normal);
+            Assert.AreEqual(expected_Fare_Premium, fare_Premium);
+
+
+
+
         }
 
     }
